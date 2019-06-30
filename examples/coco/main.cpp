56c4e7d9e9649ecabaaad0c1d109dcaaae57be1d
mohamedali1995 <muhamedali166@gmail.com>
2019-06-30 23:17:02 +0200

    Updating coco example.

--- examples/coco/main.cpp
+++ examples/coco/main.cpp
@@ -1,57 +1,67 @@
 //
 // Created by sergio on 16/05/19.
 //
-
+#include <iostream>
 #include "../../include/Model.h"
 #include "../../include/Tensor.h"
 #include <opencv2/opencv.hpp>
 #include <numeric>
 #include <iomanip>
 
-int main() {
-    Model model("../ssd_inception/frozen_inference_graph.pb");
-    auto outNames1 = new Tensor(model, "num_detections");
-    auto outNames2 = new Tensor(model, "detection_scores");
-    auto outNames3 = new Tensor(model, "detection_boxes");
-    auto outNames4 = new Tensor(model, "detection_classes");
 
-    auto inpName = new Tensor(model, "image_tensor");
+int main(){
+    Model model("../frozen_model.pb");
+    auto outName = new Tensor(model, "output");
+    auto inpName1 = new Tensor(model, "input_1");
+    auto inpName2 = new Tensor(model, "input_2");
+    auto inpName3 = new Tensor(model, "Placeholder_2");
+    auto inpName4 = new Tensor(model, "Placeholder");
+
+    cv::Mat img1;
+    cv::Mat img2;
+    cv::Mat inp1;
+    cv::Mat inp2;
+
+    img1 = cv::imread("/media/syrix/programms/projects/GP/Intelligent_Frame_Skipping/src/libs/cppflow/examples/coco/2201.png", CV_LOAD_IMAGE_COLOR);
+   	int rows1 = img1.rows;
+    int cols1 = img1.cols;
+
+    img2 = cv::imread("/media/syrix/programms/projects/GP/Intelligent_Frame_Skipping/src/libs/cppflow/examples/coco/2201.png", CV_LOAD_IMAGE_COLOR);
+   	int rows2 = img2.rows;
+    int cols2 = img2.cols;
 
 
-    // Read image
-    cv::Mat img, inp;
-    img = cv::imread("../test.jpg", CV_LOAD_IMAGE_COLOR);
 
-    int rows = img.rows;
-    int cols = img.cols;
+    cv::resize(img1, inp1, cv::Size(256, 256));
+    cv::resize(img2, inp2, cv::Size(256, 256));
 
-    cv::resize(img, inp, cv::Size(300, 300));
-    cv::cvtColor(inp, inp, CV_BGR2RGB);
+    cv::cvtColor(inp1, inp1, CV_BGR2RGB);
+    cv::cvtColor(inp2, inp2, CV_BGR2RGB);
 
-    // Put image in Tensor
-    std::vector<uint8_t > img_data;
-    img_data.assign(inp.data, inp.data + inp.total() * inp.channels());
-    inpName->set_data(img_data, {1, 300, 300, 3});
+    cout<<"image height: "<<rows1<<std::endl;
+    // std::vector<float > img1_data;
+    // img1_data.assign(inp1.data, inp1.data + inp1.total() * inp1.channels());
 
-    model.run(inpName, {outNames1, outNames2, outNames3, outNames4});
+    // std::vector<float > img2_data;
+    // img2_data.assign(inp2.data, inp2.data + inp2.total() * inp2.channels());
 
-    // Visualize detected bounding boxes.
-    int num_detections = (int)outNames1->get_data<float>()[0];
-    for (int i=0; i<num_detections; i++) {
-        int classId = (int)outNames4->get_data<float>()[i];
-        float score = outNames2->get_data<float>()[i];
-        auto bbox_data = outNames3->get_data<float>();
-        std::vector<float> bbox = {bbox_data[i*4], bbox_data[i*4+1], bbox_data[i*4+2], bbox_data[i*4+3]};
-        if (score > 0.3) {
-            float x = bbox[1] * cols;
-            float y = bbox[0] * rows;
-            float right = bbox[3] * cols;
-            float bottom = bbox[2] * rows;
 
-            cv::rectangle(img, {(int)x, (int)y}, {(int)right, (int)bottom}, {125, 255, 51}, 2);
-        }
-    }
+    // inpName1->set_data(img1_data, {1, 256, 256, 3});
+    // inpName2->set_data(img2_data, {1, 256, 256, 3});
 
-    cv::imshow("Image", img);
-    cv::waitKey(0);
+
+    // std::vector<float > hold_prob;
+    // hold_prob.push_back(1.0);
+    // inpName3->set_data(hold_prob, {1});
+
+    // std::vector<int> is_training;
+    // is_training.push_back(1);
+    // inpName4->set_data((bool)is_training, {1});
+
+
+
+    // model.run({inpName1, inpName2, inpName3, inpName4}, outName);
+    // std::cout<<"output: "<< outName->get_data<float>()[0] <<std::endl;
+
+return 0;
 }
